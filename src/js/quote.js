91b09c7ba46e3675173fe0221d75c41f74f774c7
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-04 19:17:04 +0100

    rewrite the module with vanilla JavaScript

--- src/js/quote.js
+++ src/js/quote.js
@@ -1,41 +1,47 @@
-import {loadNextImage} from './background.js';
+import { loadNextImage } from './background.js';
+import { showQuotesButton, showProverbsButton, proverbQuoteContent, author, 
+  nextQuoteButton, nextProverbButton } from './proverb.js';
 
-//cache DOM
-const $showQuotes = $('#show-quotes');
-const $showProverbs = $('#show-proverbs');
-const $proverb = $('#proverb');
-const $nextProverb = $('#next');
-const $nextQuote = $('#next-quote');
-const $author = $('#author');
+//declare variables
+let uniqueUrl = 1;
 
 //bind events
-$showQuotes.on('click', showQuotes);
-$nextQuote.on('click', showNextQuote);
+showQuotesButton.addEventListener('click', showQuotes);
+nextQuoteButton.addEventListener('click', showNextQuote);
 
 //function declarations
 function callAjax() {
-    $.ajax({
-    url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-    cache: false 
-    /* when cache set to false it forces the requested page not to be cached by the browser */
-  })
-  .done(function(responce) {
-    $proverb.html(responce[0].content);
-    $author.html(responce[0].title);
-  })
-  .fail(function(jqXHR, textStatus, errorThrown) {
-    $proverb.html('Connection error');
-  });
+  const httpRequest = new XMLHttpRequest();
+  /* unique URL is used so that the quote won't be cached by the browser */
+  httpRequest.open('GET', `https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1?sig=${uniqueUrl}`);
+
+  httpRequest.onload = function() {
+    if (httpRequest.status >= 200 && httpRequest.status < 400) {
+      const quote = JSON.parse(httpRequest.responseText);
+      proverbQuoteContent.innerHTML = quote[0].content;
+      author.textContent = quote[0].title;
+    } else {
+      /* handle server connection error */
+      proverbQuoteContent.textContent = 'Server returned an error';
+    }
+  };
+  /* handle internet connection error */
+  httpRequest.onerror = function() {
+    proverbQuoteContent.textContent = 'Connection error';
+  };
+
+  httpRequest.send();
+  uniqueUrl++;
 }
 
 function showQuotes() {
-  $proverb.html('');
+  proverbQuoteContent.textContent = '';
+  proverbQuoteContent.classList.remove('largefont');
+  showProverbsButton.classList.remove('active');
+  showQuotesButton.classList.add('active');
+  nextProverbButton.classList.add('is-not-active');
+  nextQuoteButton.classList.remove('is-not-active');
   showNextQuote();
-  $proverb.removeClass('largefont');
-  $showProverbs.removeClass('active');
-  $(this).addClass('active');
-  $nextProverb.hide();
-  $nextQuote.show();
 }
 
 function showNextQuote() {
