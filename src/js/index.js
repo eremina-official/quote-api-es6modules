aa294ed9bed8bbf586ad849446f7856b16296765
Marina Eremina <eremina.jobmail@gmail.com>
2019-04-16 23:09:44 +0200

    add loading handling

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,10 @@ import './proverb.js';
 import './quote.js';
 import '../css/stylesheet.css';
 import '../css/style-proverb.css';
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    const loader = document.querySelector('.js-loader');
+    loader.classList.add('loader-is-hidden');
+  }, 300);
+});
