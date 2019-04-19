60ad94783d0dbc6c452ee14bc6f25df4fb15e887
Marina Eremina <eremina.jobmail@gmail.com>
2019-04-19 19:59:22 +0200

    refactor function called on window load

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,20 @@ import './proverb.js';
 import './quote.js';
 import '../css/stylesheet.css';
 import '../css/style-proverb.css';
+import { loadImage } from './background.js';
+import { showProverb } from './proverb.js';
 
-window.addEventListener('load', () => {
+window.addEventListener('load', init);
+
+function init() {
+  showLoader();
+  loadImage();
+  showProverb();
+}
+
+function showLoader() {
   setTimeout(() => {
     const loader = document.querySelector('.js-loader');
     loader.classList.add('loader-is-hidden');
-  }, 300);
-});
+  }, 1000);
+}
